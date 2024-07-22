d88c926a8846fad045685b7d296be1cb9afc939f
Kohler Elias - Studierendenaccount <elias.kohler@student.uibk.ac.at>
2024-07-22 12:46:48 +0200

    refactor: improve invoke command & error handling, fix some sonarlint issues

--- src/main/java/at/ac/uibk/dps/cirrina/utils/BuildVersion.java
+++ src/main/java/at/ac/uibk/dps/cirrina/utils/BuildVersion.java
@@ -2,6 +2,9 @@ package at.ac.uibk.dps.cirrina.utils;
 
 public class BuildVersion {
 
+  private BuildVersion() {
+  }
+
   public static String getBuildVersion() {
     return BuildVersion.class.getPackage().getImplementationVersion();
   }
